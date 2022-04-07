0130a6bfee041de445282dbaba2fa35849f0b39a
queziafiladelfo <queziafiladelfo@gmail.com>
2022-04-07 19:17:51 -0300

    Modificando slides

--- js/main.js
+++ js/main.js
@@ -14,3 +14,31 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+
+    spaceBetween: 5,
+    slidesPerView: 2,
+    loop: true,
+    freeMode: true,
+    loopAdditionalSlides: 5,
+    speed: 500,
+  
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 5,
+            //slidesPerGroup: 5,
+            freeMode: true,
+        }
+    }
+
+});
